cb221320c3f620113aa69d9ae283da69abf1f895
Sarkipraizy <Praizyatong@gmail.com>
2023-10-03 22:51:03 +0100

    express

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const itemRouter = require('./Items/items_router')
+const signUpRoute = require('./Users/user_router')
+
+const app = express();
+const PORT = 5000;
+const HOSTNAME = 'localhost';
+
+// for rendering stactic html file
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+// all items endpoint
+app.use('/api', itemRouter)
+
+// for signing up users
+app.use(express.json())
+app.use('/', signUpRoute)
+
+
+app.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is listening at port:${PORT}`);
+});
